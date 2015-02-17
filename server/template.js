bd1566386596c39e9db19e8db9dc23cfa4183962
Ap0c <Ap0c@users.noreply.github.com>
2015-02-17 17:56:20 +0000

    Refactored template engine code.

--- server/template.js
+++ server/template.js
@@ -1,5 +1,20 @@
 // ---------- Functions ---------- //
 
+// Attempts to replace a specific variable in a template.
+var replaceVariable = function (page, variables, result) {
+
+	var variable = result[1].trim();
+
+	if (variable in variables) {
+		var value = variables[variable];
+		page.data = page.data.replace(result[0], value);
+	} else {
+		page.success = false;
+	}
+
+}
+
+
 // Replaces the variables in the template with their values.
 var fillVariables = function (page, variables) {
 
@@ -7,14 +22,7 @@ var fillVariables = function (page, variables) {
 
 	while ((result = re.exec(page.data)) && page.success == true) {
 
-		var variable = result[1].trim();
-
-		if (variable in variables) {
-			var value = variables[variable];
-			page.data = page.data.replace(result[0], value);
-		} else {
-			page.success = false;
-		}
+		replaceVariable(page, variables, result);
 
 	}
 
